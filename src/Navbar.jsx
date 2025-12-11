af3546eaec6d674ffae35f7dc2e1507247755482
faraz khan <mevfarazkhan@gmail.com>
2025-12-11 19:46:57 +0530

    navbar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,86 +1,89 @@
-import React from 'react'
-import { Link, Outlet, useLocation } from 'react-router-dom'
+import React from 'react';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import { FaCartShopping } from "react-icons/fa6";
-import logo from './Logo/Gemini_Generated_Image_2laa422laa422laa.png'
+import logo from './Logo/Gemini_Generated_Image_2laa422laa422laa.png';
+
 export default function Navbar({ len }) {
     const location = useLocation();
 
     return (
         <>
-            <nav className="navbar navbar-expand-sm fixed-top mt-3">
-                <div
-                    className="container-fluid py-2 rounded mx-4"
-                    style={{
-                        backgroundColor:
-                            location.pathname === "/"
-                                ? "rgba(0,0,0,0.797)"
-                                : "rgba(57,57,57,0.797)"
-                    }}
-                >
-                 {/* <span '></span> */}
-                 <Link to={'/'} className='navbar-brand text-light'><img src={logo} alt=""  width={"40px"}  className='llimg me-3' /><span className='nlogo'>ΓΛRΛZ</span></Link>
+           <nav className="navbar navbar-expand-sm fixed-top mt-3">
+    <div
+        className="container-fluid py-2 rounded mx-4"
+        style={{
+            backgroundColor:
+                location.pathname === "/"
+                    ? "rgba(0,0,0,0.797)"
+                    : "rgba(57,57,57,0.797)"
+        }}
+    >
 
-                    {/* TOGGLER */}
-                    <button
-                        className="navbar-toggler bg-light"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#collapsibleNavbar"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+        {/* TOP BAR (LOGO - TOGGLER - CART) */}
+        <div className="d-flex align-items-center w-100 position-relative">
 
-                    {/* COLLAPSE MENU */}
-                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                        <ul className="navbar-nav mx-auto text-center">
+            {/* LEFT - LOGO */}
+            <Link to="/" className="navbar-brand text-light">
+                <img src={logo} width="40px" className="llimg me-3" />
+                <span className="nlogo">ΓΛRΛZ</span>
+            </Link>
 
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link text-light" to="/">Home</Link>
-                            </li>
+            {/* CENTER - TOGGLER (ONLY MOBILE) */}
+            <button
+                className="navbar-toggler mx-4 bg-light position-absolute start-50 translate-middle-x d-sm-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#mainNav"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
 
-                            <li className="nav-item dropdown mx-3">
-                                <Link className="nav-link dropdown-toggle text-light" role="button" data-bs-toggle="dropdown">
-                                    Category
-                                </Link>
-                                <ul className="dropdown-menu nav-bg">
-                                    <li><Link className="dropdown-item text-light" to="/products/shirts">Shirts</Link></li>
-                                    <li><Link className="dropdown-item text-light" to="/products/t-shirts">T-Shirts</Link></li>
-                                    <li><Link className="dropdown-item text-light" to="/products/jeans">Jeans</Link></li>
-                                    <li><Link className="dropdown-item text-light" to="/products/shoes">Shoes</Link></li>
-                                </ul>
-                            </li>
+            {/* RIGHT - CART */}
+           
 
-                            <li className="nav-item mx-3">
-                                <Link className="nav-link text-light" to="#">Service</Link>
-                            </li>
+        {/* SINGLE MENU FOR BOTH MOBILE & DESKTOP */}
+        <div className="collapse navbar-collapse mt-0" id="mainNav">
 
-                            {/* CART INSIDE TOGGLE (visible only on mobile) */}
-                            <li className="nav-item mt-3 d-sm-none">
-                                <Link to="/cart" className="d-flex justify-content-center">
-                                    <button type="button" className="btn position-relative">
-                                        <FaCartShopping className="text-light fs-3" />
-                                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                            {len}
-                                        </span>
-                                    </button>
-                                </Link>
-                            </li>
+            <ul className="navbar-nav mx-sm-auto text-center d-sm-flex gap-sm-4">
 
-                        </ul>
-                    </div>
+                <li className="nav-item">
+                    <Link className="nav-link text-light" to="/">Home</Link>
+                </li>
 
-                    {/* CART ON RIGHT (visible only on desktop) */}
-                    <Link to="/cart" className="d-none d-sm-flex">
-                        <button type="button" className="btn position-relative">
-                            <FaCartShopping className="text-light fs-3" />
-                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                {len}
-                            </span>
-                        </button>
+                <li className="nav-item dropdown">
+                    <Link className="nav-link dropdown-toggle text-light" data-bs-toggle="dropdown">
+                        Category
                     </Link>
+                    <ul className="dropdown-menu nav-bg">
+                        <li><Link className="dropdown-item text-light" to="/products/shirts">Shirts</Link></li>
+                        <li><Link className="dropdown-item text-light" to="/products/t-shirts">T-Shirts</Link></li>
+                        <li><Link className="dropdown-item text-light" to="/products/jeans">Jeans</Link></li>
+                        <li><Link className="dropdown-item text-light" to="/products/shoes">Shoes</Link></li>
+                    </ul>
+                </li>
 
-                </div>
-            </nav>
+                <li className="nav-item">
+                    <Link className="nav-link text-light" to="#">Service</Link>
+                </li>
+
+            </ul>
+
+        </div>
+         <div className="ms-auto mx-2">
+                <Link to="/cart">
+                    <button className="btn position-relative">
+                        <FaCartShopping className="text-light fs-3" />
+                        <span className="position-absolute top-0 start-100  translate-middle badge bg-danger">
+                            {len}
+                        </span>
+                    </button>
+                </Link>
+            </div>
+        </div>
+
+    </div>
+</nav>
+
 
             <Outlet />
         </>
