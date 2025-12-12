e99bc7f428d407802990bfade70343236df3c371
faraz khan <mevfarazkhan@gmail.com>
2025-12-12 17:53:00 +0530

    margin

--- src/Orderplace.jsx
+++ src/Orderplace.jsx
@@ -7,13 +7,20 @@ export default function Orderplace() {
   setTimeout(()=>{
 
     nav("/")
-  },3000)
+  },4000)
 
   
 
   return (
+    <>
     <div className='confp mt-5 '>
+      <div className="row mt-5"></div>
+      <div className="row mt-5"></div>
+      {/* <div className="row mt-5"></div> */}
+      {/* <div className="row mt-5"></div>   */}
+      
         <img className='mt-5 img-fluid rounded-5'  src="https://cdn.dribbble.com/userupload/25034830/file/original-eabff5d98a68ab76901346d02b4075d7.gif" alt="" />
     </div>
+    </>
   )
 }
