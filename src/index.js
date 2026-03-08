395c380f50bdb37c350c94bcda97fd982a7083c9
faraz khan <mevfarazkhan@gmail.com>
2026-03-08 13:07:20 +0530

    remove

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import '../node_modules/aos/dist/aos.css'
-import reportWebVitals from './reportWebVitals';
 import Browserr from './Browserr';
 import './Style.css'
 
